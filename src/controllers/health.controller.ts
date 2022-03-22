c9fc41f2cda1900c2c546677cb58cee3316009a7
Leung Yau Ming <leungyauming2005@gmail.com>
2022-03-22 17:27:53 +0800

    create health endpoint controller

---
+++ src/controllers/health.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+import { genRes } from '~/helpers/response.helper';
+
+@Controller('/health')
+export class HealthController {
+  @Get()
+  checkHealth() {
+    return genRes(true, 200, { api: 'good' });
+  }
+}
