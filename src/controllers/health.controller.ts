3d7714596e4890ecabeef9db4a70598c43d33e13
Leung Yau Ming <leungyauming2005@gmail.com>
2022-03-22 18:30:24 +0800

    add health status check for database

--- src/controllers/health.controller.ts
+++ src/controllers/health.controller.ts
@@ -1,10 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
 import { genRes } from '~/helpers/response.helper';
+import { Connection } from 'mongoose';
 
 @Controller('/health')
 export class HealthController {
+  constructor(@InjectConnection() private conn: Connection) {}
+
   @Get()
   checkHealth() {
-    return genRes(true, 200, { api: 'up' });
+    return genRes(true, 200, {
+      api: 'up',
+      db: this.conn.readyState === 1 ? 'up' : 'down',
+    });
   }
 }
