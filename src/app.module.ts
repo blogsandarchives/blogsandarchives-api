a6cf67aaf4cd85880ee38d6a80b33b1bb9b89513
Leung Yau Ming <leungyauming2005@gmail.com>
2022-03-22 17:51:43 +0800

    setup config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from '~/app.controller';
 import { AppService } from '~/app.service';
 import { HealthController } from '~/controllers/health.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+  ],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
