c81fa0e18f2bc70ddcdfe9c5a8e5666ab0725e32
Leung Yau Ming <leungyauming2005@gmail.com>
2022-03-29 21:03:31 +0800

    add health check for MongoDB

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 import { genRes } from '~/response.helper';
 
 @Controller('health')
 export class HealthController {
+  constructor(@InjectConnection() private mongoDbConn: Connection) {}
+
   @Get()
   get() {
     return genRes(true, HttpStatus.OK, {
       api: 'up',
+      mongodb: this.mongoDbConn.readyState === 1 ? 'up' : 'down',
     });
   }
 }
