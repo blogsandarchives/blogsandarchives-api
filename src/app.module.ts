582ff132b0d660936fd908d41926484f7b9cfc4d
Leung Yau Ming <leungyauming2005@gmail.com>
2022-03-29 20:56:47 +0800

    setup mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { HealthController } from './health/health.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const addr = configService.get('MONGODB_ADDR') || '127.0.0.1';
+        const port = parseInt(configService.get('MONGODB_PORT')) || 27017;
+
+        const user = configService.get('MONGODB_USER') || 'blogsandarchives';
+        const password =
+          configService.get('MONGODB_PASSWORD') || 'blogsandarchives';
+
+        const db = configService.get('MONGODB_DB') || 'blogsandarchives';
+        const authSource = configService.get('MONGODB_AUTH_SRC') || 'admin';
+
+        return {
+          uri: `mongodb://${user}:${password}@${addr}:${port}/${db}?authSource=${authSource}`,
+        };
+      },
+    }),
+  ],
   controllers: [HealthController],
   providers: [],
 })
