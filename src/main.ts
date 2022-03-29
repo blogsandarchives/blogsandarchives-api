0de72537cf5f9e620e66078e937a945b0652f496
Leung Yau Ming <leungyauming2005@gmail.com>
2022-03-29 20:42:08 +0800

    setup config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  const addr = configService.get<string>('BIND_ADDR') || '0.0.0.0';
+  const port = parseInt(configService.get('BIND_PORT')) || 80;
+  await app.listen(port, addr);
 }
 bootstrap();
